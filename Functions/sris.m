80853dce5a7282e12994bd2b233e1e6d4bab09d7
coleschuelke <coleschuelke@gmail.com>
2025-11-21 00:43:54 -0600

    smoother doesnt really work

--- Functions/sris.m
+++ Functions/sris.m
@@ -1,28 +1,33 @@
-function [t_out, x_out, P_out] = sris(F, Gamma, H, Q, R, xhat0, P0, z)
+function [t_out, x_out, P_out, v_out, Pvv_out] = sris(F, Gamma, H, Q, R, xhat0, P0, z)
 %SRIS Square-root Information Smoother
 
 % Will need to save matrices on the forward pass
 
-%% Run the SRIF Forward
 % Characterize the data
 nx = length(xhat0);
 nz = size(z, 2);
 nv = size(Q, 1);
 num_meas = size(z, 1)/size(H, 1);
 
-% Initialize outputs (may not be the same for sris as srif)
-t_out = [0 repelem(1:num_meas, 2)].';
-x_out = zeros(num_meas*2 + 1, length(xhat0));
-x_out(1, :) = xhat0;
-P_out = zeros([size(P0), num_meas*2 + 1]);
-P_out(:, :, 1) = P0;
-if nargout > 3
-    nis = zeros(num_meas, 1);
-end
+% Initialize forward pass outputs
+t_out = [0 1:num_meas].';
+x_forward = zeros(num_meas*2 + 1, length(xhat0));
+x_forward(1, :) = xhat0;
+P_forward = zeros([size(P0), num_meas*2 + 1]);
+P_forward(:, :, 1) = P0;
 
 % Initialize saving arrays for smoothing operations
-Rvv_bar = zeros(nv, nv, num_meas);
-Rvx_bar = zeros(nv, nx, num_meas);
+Rvv_bar_forward = zeros(nv, nv, num_meas);
+Rvx_bar_forward = zeros(nv, nx, num_meas);
+Rxx_bar_forward = zeros(nx, nx, num_meas);
+zx_forward = zeros(num_meas, nx);
+zv_bar_forward = zeros(num_meas, nv);
+
+% Initialize the smoother outputs
+x_out = zeros(num_meas + 1, nx);
+P_out = zeros(nx, nx, num_meas + 1);
+v_out = zeros(num_meas+1, nv);
+Pvv_out = zeros(nv, nv, num_meas + 1);
 
 % Compute some constants
 Ra = chol(R); % This is not necessarily the same for every measurement in general
@@ -37,6 +42,7 @@ vhat = zeros(size(Q, 1), 1);
 zv = Rvv*vhat;
 zx = Rxx*xhat0;
 
+%% Run the SRIF Forward
 for k=1:num_meas
 
     % Propagate
@@ -48,12 +54,16 @@ for k=1:num_meas
     zb = Qb.'*[zeros(nv, 1); bottom];
     
     % Extract block elements from matrices
+    zv_bar_forward(k, :) = zb(1:nv).'; % Save for smoothing
     zx_bar = zb(end-nx+1:end); % I hate matlab indexing
     Rxx_bar = Rb(end-nx+1:end, end-nx+1:end);
+    Rxx_bar_forward(:, :, k) = Rxx_bar;
+    Rvv_bar_forward(:, :, k) = Rb(1:nv, 1:nv); % Save for smoothing
+    Rvx_bar_forward(:, :, k) = Rb(1:nv ,end-nx+1:end);
 
     % Save P_bar and x_bar
-    x_out(2*k, :) = Rxx_bar\zx_bar;
-    P_out(:, :, 2*k) = (Rxx_bar.'\eye(nx))*(Rxx_bar\eye(nx)); % Computationally efficient implementation
+    x_forward(2*k, :) = Rxx_bar\zx_bar;
+    P_forward(:, :, 2*k) = (Rxx_bar.'\eye(nx))*(Rxx_bar\eye(nx)); % Computationally efficient implementation
 
     % Update
     zk = z(k, :).';
@@ -67,19 +77,67 @@ for k=1:num_meas
     % Extract block elements from matrices
     Rxx = Rc(1:nx, 1:nx); 
     zx = zc(1:nx);
-
-    % Compute NIS
-    if nargout > 3
-        zr = zc(end-nz+1:end);
-        nis(k) = zr.'*zr;
-    end
+    zx_forward(k, :) = zx; % Save for smoothing
 
     % Save P and x_hat
-    x_out(2*k + 1, :) = Rxx\zx;
-    P_out(:, :, 2*k + 1) = (Rxx.'\eye(nx))*(Rxx\eye(nx));
+    x_forward(2*k + 1, :) = Rxx\zx;
+    P_forward(:, :, 2*k + 1) = (Rxx.'\eye(nx))*(Rxx\eye(nx));
 
 end
 
-% This is going to be so quick to implement
+%% Run the smoother backwards
+% Save the final forward estimates
+x_out(end, :) = x_forward(end, :);
+P_out(:, :, end) = P_forward(:, :, end);
+
+% Start with the final values
+Rvv_bar = Rvv_bar_forward(:, :, end);
+Rvx_bar = Rvx_bar_forward(:, :, end);
+zx = zx_forward(end, :).';
+zv = zv_bar_forward(end, :).';
+
+% Iterate
+for i=num_meas:-1:1
+
+    % Build the block matrix
+    upper_left = Rvv_bar + Rvx_bar*Gamma;
+    upper_right = Rvx_bar*F;
+    lower_left = Rxx*Gamma;
+    lower_right = Rxx*F;
+    block_smooth = [upper_left, upper_right; lower_left, lower_right];
+
+    % Build the z vector
+    z_smooth = [zv - Rvx_bar*G*u; zx - Rxx*G*u];
+
+    % Perform the QR Factorization to isolate x
+    [Q_smooth, R_smooth] = qr(block_smooth);
+    z_smooth_post = Q_smooth.'*z_smooth;
+
+    % Extract the relevant values
+    Rvv_star = R_smooth(1:nv, 1:nv);
+    Rvx_star = R_smooth(1:nv, end-nx+1:end);
+    Rxx_star = R_smooth(end-nx+1:end, end-nx+1:end);
+    zv_star = z_smooth_post(1:nv);
+    zx_star = z_smooth_post(end-nx+1:end);
+
+    % Calculate the smoothed estimates
+    x_star = Rxx_star\zx_star;
+    v_star = Rvv_star\(zv_star - Rvx_star*x_star);
+    P_star = Rxx_star\inv(Rxx_star).';
+    % Pvv_star = 
+
+    % Save smoothed estimates
+    x_out(i, :) = x_star;
+    P_out(:, :, i) = P_star;
+    v_out(i, :) = v_star;
+    % Pvv_out(:, :, i)
+
+    % Update for next iteration
+    Rvv_bar = Rvv_bar_forward(:, :, i); % Use from forward pass
+    Rvx_bar = Rvx_bar_forward(:, :, i); % Use from forward pass
+    zx = zx_forward(i, :).'; % Use from last smoothing iteration
+    zv = zv_bar_forward(i, :).'; % Use from forward pass
+    
+end
 
 end
---
+++ PS6/P3.m
@@ -0,0 +1,24 @@
+clear variables;
+close all;
+clc;
+
+addpath(['..\Functions\']);
+
+% Load the matrices into the workspace
+run('kf_example03b.m');
+
+% Kalman Filter
+[t_kf, x_kf, P_kf, nis_kf] = kalman_filter(Fk, Gammak, Hk, Qk, Rk, xhat0, P0, zhist);
+
+% Square-root Information Filter
+[t_srif, x_srif, P_srif, nis_srif] = srif(Fk, Gammak, Hk, Qk, Rk, xhat0, P0, zhist);
+
+% Square-root Information Smoother
+
+[t_sris, x_sris, P_sris, ~, ~] = sris(Fk, Gammak, Hk, Qk, Rk, xhat0, P0, zhist);
+
+
+% Plotting
+plot_kf(t_kf, x_kf, P_kf, [], [], 3);
+plot_kf(t_srif, x_srif, P_srif, [], [], 3);
+plot_kf(t_sris, x_sris, P_sris, [], [], 3);
